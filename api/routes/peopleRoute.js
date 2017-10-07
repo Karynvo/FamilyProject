bbb0c7e3bad03d9cf56851d0c502acde28de2d29
Karyn Vo <vkaryn@gmail.com>
2017-10-06 22:11:16 -0700

    refactoring

--- api/routes/peopleRoute.js
+++ api/routes/peopleRoute.js
@@ -1,13 +1,13 @@
 'use strict';
 module.exports = function(app){
-	var peopleList = require('../controllers/peopleController');
+	var person = require('../controllers/peopleController');
 
 	app.route('/people')
-		.get(peopleList.list_all_people)
-		.post(peopleList.create_a_person);
+		.get(person.list_all_people)
+		.post(person.create_a_person);
 
 	app.route('/people/:personId')
-		.get(peopleList.get_a_person)
-		.put(peopleList.update_a_person)
-		.delete(peopleList.delete_a_person);
+		.get(person.get_a_person)
+		.put(person.update_a_person)
+		.delete(person.delete_a_person);
 };
